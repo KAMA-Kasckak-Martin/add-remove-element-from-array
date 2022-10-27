b98e25ccaab61c1059db00e168cf4a66d797ad16
martinkascak <m.kascak93@gmail.com>
2022-10-27 15:56:18 +0200

    add finisho

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
+package com.journaldev.java;
+import java.util.Arrays;
+
+public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = new int[]{1,2,3,4,5};
+        int[] arr_new = new int[arr.length-1];
+        int j=3;
+        for(int i=0, k=0;i<arr.length;i++){
+            if(arr[i]!=j){
+                arr_new[k]=arr[i];
+                k++;
+            }
+        }
+        System.out.println("Before deletion :" + Arrays.toString(arr));
+        System.out.println("After deletion :" + Arrays.toString(arr_new));
+
     }
 }
